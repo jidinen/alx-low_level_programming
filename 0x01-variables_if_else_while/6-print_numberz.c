a8d7c4511d589e4666f706fcffaeff428ac35a41
jidinen <gofwenjidinen05@gmail.com>
2023-10-06 19:59:21 +0100

    if-else,variable,while loops

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *
+ *
+ *Return: Always 0 (Success)
+ *
+ *
+ */
+
+int main(void)
+{
+int iox = 48;
+while (iox < 58)
+{
+putchar(iox);
+iox++;
+}
+putchar('\n');
+return (O);
+}
