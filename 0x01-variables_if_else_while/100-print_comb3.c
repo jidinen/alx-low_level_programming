5c7d92733a25e5be59f09bf476bd114538f27983
jidinen <gofwenjidinen05@gmail.com>
2023-10-06 22:07:37 +0100

    ooo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+int i;
+int j;
+for(i=0; i<10; i++)
+{
+for(j=i+1; j<10; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+if (j != 56 || i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+}
+putchar('\n');
+return (0);
+}
