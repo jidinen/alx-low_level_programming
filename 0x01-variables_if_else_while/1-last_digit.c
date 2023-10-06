9279920eeda3a696ddae8ac40702981ce3ead6ab
jidinen <gofwenjidinen05@gmail.com>
2023-10-06 18:52:03 +0100

    ii

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,15 +26,15 @@ n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
 if (last_digit > 5)
 {
-printf("the last digit of %d is %d and is greater than 5\n", n, last_digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else if (last_digit == 0)
 {
-printf("the last digit of %d is %d and is 0\n", n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else if (last_digit < 6)
 {
-printf("the last digit of %d is %d and is less than 6 and 0\n", n, last_digit);
+printf("Last digit of %d is %d and is less than 6 and 0\n", n, last_digit);
 }
 return (0);
 }
