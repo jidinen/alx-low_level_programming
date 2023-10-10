d5c75674fda5a4b4fb7998651f492e19f4537195
jidinen <gofwenjidinen05@gmail.com>
2023-10-10 21:27:33 +0100

    x

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,36 +16,33 @@
  *
  */
 
-void times_table(void)
-{
-int i;
-int j;
-int result;
-int x;
-int y;
-for (i =0; i <= 9; i++)
-{
-for (j = 0; j <= 9; i++)
-{
-result = i*j;
-if (result > 9)
-{
+#include <stdio.h>
+
+
+void times_table(void) {
+int i, j, result, x, y;
+for (i = 0; i <= 9; i++) {
+for (j = 0; j <= 9; j++) {
+result = i * j;
+
+ if (result > 9) {
 x = result % 10;
-y = (y-x) / 10;
-_putchar(44);
-_putchar(32);
-_putchar(y + 'O');
-_putchar(x + 'O');
-}
-else if (j != 0)
-{
-_putchar(44);
-_putchar(32);
-_putchar(32);
-}
-_putchar(result + 'O');
-}
-}
+y = (result - x) / 10;
+
+_putchar(',');
+_putchar(' ');
+_putchar(y + '0');
+_putchar(x + '0');
+} else {
+if (j != 0) {
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+ }
+ _putchar(result + '0');
+ }
+ }
 _putchar('\n');
+}
 }
 
