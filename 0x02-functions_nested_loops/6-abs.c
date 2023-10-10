66adeed4de5a36170927ec85a835154b6287a2b1
jidinen <gofwenjidinen05@gmail.com>
2023-10-10 17:26:26 +0100

    x

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *_abs - returns absolute value for a interger
+ *
+ *
+ *
+ *@int: the number
+ *
+ *
+ *
+ *Return: absolute value
+ *
+ *
+ *
+ */
+int _abs(int a)
+{
+int j; 
+int result;
+j = -1;
+if (a > 0)
+{
+return (a);
+}
+else if ( a < 0)
+{
+result = a * j;
+return (result);
+}
+return (a);
+}
+
