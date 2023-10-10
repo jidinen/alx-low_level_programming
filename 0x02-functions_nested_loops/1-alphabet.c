9625b54a113615e4fca6b353350b64cd2d6cc5f2
jidinen <gofwenjidinen05@gmail.com>
2023-10-10 14:47:54 +0100

    x

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ *print_alphabet - prints all the alphabets in lowercase
+ *
+ *Return: void
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+int i;
+i = 97;
+while (i <= 122)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
