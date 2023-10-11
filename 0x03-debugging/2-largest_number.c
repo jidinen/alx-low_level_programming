e1e9d4ee7533822fa8637ba06b3b1891c2cfd528
jidinen <gofwenjidinen05@gmail.com>
2023-10-11 17:34:59 +0100

    oo

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,15 +16,18 @@ if (a > b && a > c)
 {
 largest = a;
 }
-else if (b > a && b > c)
-{
-largest = b;
-}
-
-else
+else if (a > b && c > a)
 {
 largest = c;
 }
 
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 return (largest);
 }
