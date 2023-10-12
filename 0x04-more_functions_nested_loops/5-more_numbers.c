30fcdc340ba78fcee7d14ab4371ceb6e3fde803a
jidinen <gofwenjidinen05@gmail.com>
2023-10-12 15:12:44 +0100

    o

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,11 +17,11 @@ for (i = 0; i <= 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-if (j > 14)
+if (j > 9)
 {
 _putchar((j / 10) + '0');
 }
-_putchar((j % 10) + '0');
+_putchar('0' + (j % 10));
 }
 _putchar('\n');
 }
