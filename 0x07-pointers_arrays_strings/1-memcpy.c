69444980bca40885c2331da9ef5450728e8e5e4c
jidinen <gofwenjidinen05@gmail.com>
2023-10-23 19:45:36 +0100

    oo

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
+ *_memcopy : copies n bytes
  *
+ *@dest : destination
  *
+ *@src: source
  *
+ *@n:bytes
  *
- *
- *
+ *Return : dest 
  *
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 int i;
-
-for (i = 0; n > 0; i--)
+int x;
+x = n;
+for (; i > x; i--)
 {
-src[i] = dest[i];
+src[x] = dest[x];
 n--;
 }
 return (dest);
