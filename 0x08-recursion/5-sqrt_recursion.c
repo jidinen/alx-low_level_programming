a02c8d935640ec37c573a925c6adfd9785f6d928
jidinen <gofwenjidinen05@gmail.com>
2023-10-26 14:12:50 +0100

    hello world

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
+ *_sqrt_recursion - prints the square root of n
  *
  *
+ *@n: the number
  *
  *
- *
- *
- *
+ *Return: the sqr roots
  *
  *
  *
@@ -17,8 +17,23 @@
 
 int _sqrt_recursion(int n)
 {
-if (n <= 0)
+int i;
+int product;
+if (n == 1)
 {
+return (1);
+}
+for (i = 0; i < n; i++)
+{
+product = i * i;
+if (product == n)
+{
+return (i);
+}
+}
+if (product < n)
+{
+return (_sqrt_recursion(n - product));
+}
 return (-1);
 }
-return _sqrt_recursion(n
