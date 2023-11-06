bef754a016a927ef0eddaa643a659c07e5250ad8
jidinen <gofwenjidinen05@gmail.com>
2023-11-06 13:57:18 +0100

    checked my code for betty error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,25 @@
 #ifndef dog_h
 #define dog_h
 
+/**
+ *struct dog - name of structure
+ *
+ *@name: first member
+ *@age:second member
+ *@owner:third member
+ *
+ *
+ *
+ *
+ */
+
 struct dog
-{
-	char *name;
-       	float age;
+{	char *name;
+	float age;
 	char *owner;
 };
 
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-#endif 
+#endif
