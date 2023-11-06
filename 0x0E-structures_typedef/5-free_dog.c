b21ae4b32690f0fe09e9e9c033bdce6fceaa941d
jidinen <gofwenjidinen05@gmail.com>
2023-11-06 15:42:42 +0100

    freeing the allocated space

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *free_dog - releases the space allocated
+ *
+ *@d: the param
+ *
+ *Return: void 
+ *
+ *
+ *
+ *
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
+
