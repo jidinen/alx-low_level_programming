a7d6593e2c86ca4da74df1a14535701a8c8a49b5
jidinen <gofwenjidinen05@gmail.com>
2023-11-21 20:52:02 +0100

    betty style correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,8 +22,6 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *newNode = (listint_t *)malloc(sizeof(listint_t));
-unsigned int inz;
-listint_t *hc;
 
 if (newNode == NULL)
 {
@@ -38,38 +36,5 @@ newNode->next = *head;
 *head = newNode;
 return (newNode);
 }
-
-
-
-
-
-
-hc = *head;
-
-inz = 0;
-
-
-while (hc != NULL)
-{
-if (idx == inz + 1)
-{
-
-listint_t *nextN = hc->next;
-
-hc->next = newNode;
-
-newNode->next = nextN;
-
-return (newNode);
-}
-inz++;
-
-hc = hc->next;
-}
-
-free(newNode);
-
 return (NULL);
 }
-
-
