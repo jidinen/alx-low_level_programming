2a4ad431a123c29a32c9a1ecf25f5cdc53bebc95
jidinen <gofwenjidinen05@gmail.com>
2023-11-21 21:19:10 +0100

    went back to the oringinal

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,10 +18,12 @@
  *Return: the newnode
  *
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *newNode = (listint_t *)malloc(sizeof(listint_t));
+unsigned int inz;
+listint_t *hc = *head;
+inz = 0;
 
 if (newNode == NULL)
 {
@@ -36,5 +38,21 @@ newNode->next = *head;
 *head = newNode;
 return (newNode);
 }
+
+
+while (hc != NULL)
+{
+
+if (idx == inz + 1)
+{
+listint_t *nextN = hc->next;
+hc->next = newNode;
+newNode->next = nextN;
+return (newNode);
+}
+inz++;
+hc = hc->next;
+}
+
 return (NULL);
 }
