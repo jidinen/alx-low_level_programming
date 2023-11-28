f3882db6f6f41bd66a6d12bda2d4e0605c6896ff
jidinen <gofwenjidinen05@gmail.com>
2023-11-28 12:53:07 +0100

    appending

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+int bytes;
+int f_x;
+int bit;
+if (!filename)
+return (-1);
+
+f_x = open(filename, O_WRONLY | O_APPEND);
+
+if (f_x == -1)
+return (-1);
+
+if (text_content)
+{
+
+for(bit = 0; text_content[bit]; bit++)
+	;
+
+bytes = write(f_x, text_content, bit);
+
+if (bytes == -1)
+return (-1);
+}
+
+close(f_x);
+
+return(1);
+}
