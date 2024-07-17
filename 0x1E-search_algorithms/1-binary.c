621860f536fe0c35cf26090570f1fd6e1406b9c6
jidinen <gofwenjidinen05@gmail.com>
2024-07-17 14:08:13 +0100

    making some changes

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,32 +10,32 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-  int counter;
-  int low;
-  int high;
-  int mid_value;
+int counter;
+int low;
+int high;
+int mid_value;
 
   /* Checking if Array is NULL */
-  if (array == NULL)
-    return (-1);
+if (array == NULL)
+return (-1);
 
-  for (low = 0, high = size - 1; high >= low;)
-    {
-      printf("Searching in array: ");
-      for (counter = low; counter < high; counter++)
-	printf("%d, ", array[counter]);
-      printf("%d\n", array[counter]);
+for (low = 0, high = size - 1; high >= low;)
+{
+printf("Searching in array: ");
+for (counter = low; counter < high; counter++)
+printf("%d, ", array[counter]);
+printf("%d\n", array[counter]);
 
-      mid_value = low + (high - low) / 2;
+mid_value = low + (high - low) / 2;
 
-      if (array[mid_value] == value)
-	return (mid_value);
+if (array[mid_value] == value)
+return (mid_value);
 
-      if (array[mid_value] < value)
-	low = mid_value + 1;
-      else
-	high = mid_value - 1;
-    }
+if (array[mid_value] < value)
+low = mid_value + 1;
+else
+high = mid_value - 1;
+}
 
-  return (-1);
+return (-1);
 }
